e3032b52cc43b2a935efd8394413b9d947d6eb1f
Yumiko Kamiyama <tototina.ruru@gmail.com>
2020-11-16 04:04:19 +0900

    add:column_Limit

--- spec/system/task.spec.rb
+++ spec/system/task.spec.rb
@@ -1,16 +1,18 @@
 require 'rails_helper'
 RSpec.describe 'タスク管理機能', type: :system do
+  let!(:task) { FactoryBot.create(:task, task_title: 'task') }
   before do
     FactoryBot.create(:task)
     FactoryBot.create(:second_task)
+    visit tasks_path
   end
   describe '新規作成機能' do
     context 'タスクを新規作成した場合' do
       it '作成したタスクが表示される' do
       visit new_task_path
-      fill_in 'Task title', with: 'タスク名'
-      fill_in 'Task description', with: 'タスク詳細'
-      click_on 'Create Task'
+      fill_in 'タイトル', with: 'タスク名'
+      fill_in '詳細', with: 'タスク詳細'
+      click_on '登録する'
       expect(page).to have_content 'タスク名'
       expect(page).to have_content 'タスク詳細'
       end
@@ -19,9 +21,14 @@ RSpec.describe 'タスク管理機能', type: :system do
   describe '一覧表示機能' do
     context '一覧画面に遷移した場合' do
       it '作成済みのタスク一覧が表示される' do
-        task = FactoryBot.create(:task, task_title: 'task')
-        visit tasks_path
         expect(page).to have_content 'task'
+      end
+    end
+    context 'タスクが作成日時の降順に並んでいる場合' do
+      it '新しいタスクが一番上に表示される' do
+        task_list = all('.task_row')
+        expect(task_list[0]).to have_content 'Factoryで作ったデフォルトのタイトル２'
+        expect(task_list[1]).to have_content 'Factoryで作ったデフォルトのタイトル１'
       end
     end
   end
